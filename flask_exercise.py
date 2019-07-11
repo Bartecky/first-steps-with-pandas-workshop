2dca039a2bfe60eb65a1ce56136d46581ae8165a
Bartłomiej Borecki <borecki.bartek@gmail.com>
2019-07-11 14:48:13 +0200

    some exercises with pandas

--- flask_exercise.py
+++ flask_exercise.py
@@ -1,31 +1,52 @@
-from flask import (
-    Flask,
-    jsonify,
-)
+from flask import Flask
 import pandas as pd
 
 app = Flask(__name__)
 
+movies = pd.read_csv('data/movies.csv')
+
 
 @app.route("/movie-titles")
 def movie_titles():
-    return jsonify(
-        {'error': 'Please implement me! I want here a list of movie titles!'}
-    )
+    result = movies['movie_title']
+    return serialize(result)
 
 
 @app.route("/worst")
 def worst():
-    return jsonify(
-        {'error': 'Please implement me! I want here a list of lowest rated movies ever!'}
+    result = (
+        movies.sort_values('imdb_score').head(5)[['movie_title', 'imdb_score']]
+
     )
+    return serialize(result)
 
 
 @app.route("/best-rated/<int:year>")
 def best_rated(year):
-    return jsonify(
-        {'error': 'Please implement me! I want here list of best rated movies in a given year!'}
+    result = (
+        movies[movies['title_year'] == year].sort_values('imdb_score', ascending=False)
+            .head(10)[['movie_title', 'imdb_score']]
     )
+    return serialize(result)
+
+
+@app.route('/most-expensive')
+def most_expensive():
+    result = (
+        movies.sort_values('budget', ascending=False).head(10)[['movie_title', 'budget']]
+    )
+    return serialize(result)
+
+
+@app.route('/most-expensive/<int:year>')
+def most_expensive_year(year):
+    result = movies[movies['title_year'] == year].sort_values('budget', ascending=False) \
+        .head(10)[['movie_title', 'budget']]
+    return serialize(result)
+
+
+def serialize(result):
+    return result.to_json(orient='records')
 
 
 if __name__ == "__main__":
